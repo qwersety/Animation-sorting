31b563c103d17c2f0d0e700fe685c7f6503edff6
qwersety <63055743+qwersety@users.noreply.github.com>
2020-06-06 12:41:12 +0500

    Update main.js
    sortposition

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", start);
 function start() {
+  // число цифр в сортировке
   let charCount=10;
-  let arrayBefore=[6,5,0,9,3,1,8,7,2,6];
-  let arrayPosition=bubbleSort(arrayBefore, 10);
-  for (let i = 0; i <= charCount; i++) {
-    arrayPosition
+  //массив для сортировки
+  let arrayBefore=[6,5,1,10,4,2,9,8,3,7];
+  // маасив для сбора данных по полученному массиву
+  // itemNumber-порядковый номер; generatedArray-сам сгенерированный массив; sortPosition-позиция в отсортированном массиве;
+  // levelPosition-позиция по вертикали (уровень размещения); leftChild, rightChild - дети в дереве; parent - родитель в дереве
   let arrayBase=[
     ['itemNumber'],
     ['generatedArray'],
@@ -14,14 +16,26 @@ function start() {
     ['levelPosition'],
     ['leftChild'],
     ['rightChild'],
+    ['parent']
   ];
+  // все до след комментария - процесс заполнения "arrayBase"
   for (let i = 1; i <= charCount; i++) {
     arrayBase[0][i]=i;
     arrayBase[1][i]=arrayBefore[i];
   }
+  let arrayPosition=bubbleSort(arrayBefore, 10);
+  for (let j = 0; j < charCount; j++)
+    for (let i = 0; i < charCount; i++) {
+      if (arrayPosition[i]+1==arrayBase[0][j+1]){
+        arrayBase[2][j+1]=i+1;
+        break;
+    }
+  }
 
 }
 
+
+// сортировка пузырьком, модернезирована под формат вывода изменения позиций чисел (на ее основе строится sortPosition)
 function bubbleSort(arr, charCount) {
   let arrayB=[];
   let arrayS=[];
@@ -33,7 +47,6 @@ function bubbleSort(arr, charCount) {
     let wasSwap = false;
     for (let j = 0, endJ = endI - i; j < endJ; j++) {
       if (arrayS[j] > arrayS[j + 1]){
-        console.log("+");
         let swap1 = arrayB[j];
         let swap2 = arrayS[j];
         arrayB[j] = arrayB[j + 1];
